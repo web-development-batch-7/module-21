d58f167a77fe37249d8add68565f332ff31718d7
arif-dev1 <aiwasim.dev1@gmail.com>
2023-02-05 21:27:36 +0600

    class 4

---
+++ class3.js
@@ -0,0 +1,16 @@
+// var num = 2;
+// if (num % 2 == 0) {
+//     console.log("Number is Even");
+// } else {
+//     console.log("Number is Odd");
+// }
+
+function evenOdd(num) {
+    if (num % 2 == 0) {
+        console.log("Number is Even");
+    } else {
+        console.log("Number is Odd");
+    }
+}
+
+evenOdd(5);
